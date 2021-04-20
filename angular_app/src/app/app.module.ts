0986629e4ff2f903f48dc2583fb33c7e95e83b3a
Em Jones <em@emtech.cloud>
2021-04-19 20:43:56 -0500

    chore(store): add ngrx

--- angular_app/src/app/app.module.ts
+++ angular_app/src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,11 @@ import { ComponentsModule } from './components/components.module';
     BrowserModule,
     AppRoutingModule,
     // Custom modules
-    ComponentsModule
+    ComponentsModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
